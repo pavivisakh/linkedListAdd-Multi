093f1b7c76a8161f7f7166a6f913ffca46a32cf6
Pavi Prasad <117369471+pavivisakh@users.noreply.github.com>
2023-12-15 19:06:03 +0530

    Update polyadd.c

--- polyadd.c
+++ polyadd.c
@@ -1,245 +1,263 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node{
- int coeff;
- int expo;
- struct node*link;
-};
+struct element
+{
+    int coeff;
+    int expo;
+    struct element *next;
+} * p1, *p2, *sum, *product;
 
-
-struct node * createNode(struct node*head,int coeff,int expo){
-    struct node*newNode=(struct node*)malloc(sizeof (struct node));
-if(newNode == NULL){
- printf("Memory allocation failed..\n");
- exit(1);
-
-}
- newNode->coeff = coeff;
- newNode->expo = expo;
- newNode->link=NULL;
-
- if(head == NULL){
- head = newNode;
-}
-else{
-  struct node* current = head;
-  while (current->link != NULL){
-    current = current->link;
-   }
-  current->link = newNode;
-}
-
-  return head;
-
-}
-
-void traverseNode(struct node* head){
-  if (head == NULL){
-    printf("Cannot traverse..\n");
-    exit(1);
-   }
-   else{
-        struct node* current= head;
-        while(current != NULL){
-         printf("%d\t",current->coeff);
- 	current = current->link;
-   }
-	printf("\n");
-	current = head;
-	while(current != NULL){
-	printf("%d\t",current->expo);
-	current = current->link;
- 	}
-     }
-  }
-
-  // adding linked list...
-
-  struct node* addLinkedList(struct node*head1 , struct node*head2){
-	struct node* resHead = NULL;
-	struct node* current1 = head1;
-	struct node* current2 = head2;
- 	while(current1 != NULL && current2 !=NULL){
-	   if(current2->expo > current2-> expo){
-	     resHead = createNode(resHead,current1->coeff,current1->expo);
-	     current1 = current1->link;
- 	   }
-	   else if (current1->expo <current2->expo){
-	     resHead = createNode(resHead,current2->coeff,current2->expo);
-	     current1 = current2->link;
-	   }
-	   else{
-	     int coeffSum = current1->coeff + current2->coeff;
-	     if(coeffSum != 0){
-		resHead = createNode(resHead,coeffSum,current1->expo);
-		   }
-	    current1 = current1->link;
-	    current2 = current2->link;
-	  }
-     }
-
-     while (current1 != NULL) {
-	resHead = createNode(resHead, current1->coeff,current1->expo);
-	current1= current1->link;
-     }
-
-     while (current2 != NULL) {
-	resHead = createNode(resHead, current2->coeff,current2->expo);
-	current2= current2->link;
-     }
-     current1 = resHead;
-     while(current1 !=NULL){
- 	current2 = current1->link;
- 	while(current2 = current1->link){
-	  if(current1->expo == current2->expo){
-	     current1->coeff = current1->coeff + current2->coeff;
-	     struct node* duplicate = current2;
-	     current1->link = current2->link;
-	     free(duplicate);
-	     current2 = current1->link;
-           }
-	   else{
-	     current2 = current2->link;
-	       }
-	    }
-	     current1 = current1->link;
-     }
-     return resHead;
- }
-
-//multiplying linked list....
-
-  struct node* multiplyLinkedList(struct node* head1, struct node* head2){
-    if(head1 == NULL|| head2 ==NULL){
-	printf("Multiplication is not possible,exiting program..");
-	exit(1);
-    }	
-	struct node* current1 = head1;
-	struct node* current2 = head2;
-	struct node*resHead = NULL;
-
-	while(current1 != NULL){
-	    current2 = head2;
-	    while(current2 != NULL){
-		int coeffMulti = current1->coeff*current2->coeff;
-		int expoSum = current1->expo + current2->expo;
-		resHead = createNode(resHead,coeffMulti,expoSum);
-		current2 = current2->link;
- 	}
+struct element *readPoly()
+{
+    int val;
+    struct element *start;
+    start = NULL;
+    struct element *current;
+    printf("Enter the number of terms in the polynomial: ");
+    scanf("%d", &val);
+    printf("Enter the coefficient and exponent of each term in decreasing order of exponents:\n");
+    for (int i = 0; i < val; i++)
+    {
+        struct element *newnode = (struct element *)malloc(sizeof(struct element));
+        newnode->next = NULL;
+        printf("Enter the coefficient of term %d: ", i + 1);
+        scanf("%d", &newnode->coeff);
+        printf("Enter the exponent of term %d: ", i + 1);
+        scanf("%d", &newnode->expo);
+        if (start == NULL)
+        {
+            start = newnode;
+            current = newnode;
+        }
+        else
+        {
+            current->next = newnode;
+            current = newnode;
+        }
     }
-
-
-// adding the resulting terms of the polynomial.....
-
-
-    current1 = resHead;
-    while(current1 !=NULL){
-	current2 = current1->link;
-	while(current2 !=NULL){
-	  if(current1->expo == current2->expo){
-		current1->coeff = current1->coeff + current2->coeff;
-		struct node* duplicate = current2;
-		current1->link = current2->link;
-		free(duplicate);
-		current2 = current1->link;
-	   }
-	   else{
-	}
-	current1 = current1->link;
-    }
-
-    return resHead;
+    return start;
 }
 
+void display(struct element *start)
+{
+    struct element *ptr = (struct element *)malloc(sizeof(struct element));
+    ptr = start;
+    while (ptr->next != NULL)
+    {
+        printf("%dx^%d + ", ptr->coeff, ptr->expo);
+        ptr = ptr->next;
+    }
+    printf("%dx^%d\n", ptr->coeff, ptr->expo);
+}
 
+struct element *addPoly(struct element *a, struct element *b)
+{
+    struct element *c;
+    struct element *ptr = (struct element *)malloc(sizeof(struct element));
+    c = ptr;
+    while (a != NULL && b != NULL)
+    {
+        if (a->expo > b->expo)
+        {
+            ptr->coeff = a->coeff;
+            ptr->expo = a->expo;
+            a = a->next;
+        }
+        else if (b->expo > a->expo)
+        {
+            ptr->coeff = b->coeff;
+            ptr->expo = b->expo;
+            b = b->next;
+        }
+        else
+        {
+            ptr->coeff = a->coeff + b->coeff;
+            ptr->expo = a->expo;
+            a = a->next;
+            b = b->next;
+        }
+        if (a || b)
+        {
+            ptr->next = (struct element *)malloc(sizeof(struct element));
+            ptr = ptr->next;
+            ptr->next = NULL;
+        }
+        else
+        {
+            ptr->next = NULL;
+        }
+    }
+    while (a != NULL || b != NULL)
+    {
+        if (a != NULL)
+        {
+            ptr->coeff = a->coeff;
+            ptr->expo = a->expo;
+            a = a->next;
+        }
+        else if (b != NULL)
+        {
+            ptr->coeff = b->coeff;
+            ptr->expo = b->expo;
+            b = b->next;
+        }
+        if (a || b)
+        {
+            ptr->next = (struct element *)malloc(sizeof(struct element));
+            ptr = ptr->next;
+            ptr->next = NULL;
+        }
+        else
+        {
+            ptr->next = NULL;
+        }
+    }
+    return c;
+}
 
+struct element *multiPoly()
+{
+    struct element *a, *b, *current, *c = NULL;
+    int tempc, tempe;
+    a = p1;
+    while (a != NULL)
+    {
+        b = p2;
+        while (b != NULL)
+        {
+            struct element *newnode = (struct element *)malloc(sizeof(struct element));
+            newnode->coeff = a->coeff * b->coeff;
+            newnode->expo = a->expo + b->expo;
+            newnode->next = NULL;
+            if (c == NULL)
+            {
+                c = newnode;
+                current = newnode;
+            }
+            else
+            {
+                current->next = newnode;
+                current = newnode;
+            }
+            b = b->next;
+        }
+        a = a->next;
+    }
+    display(c);
+    a = c;
+    while (a != NULL)
+    {
+        b = a->next;
+        while (b != NULL)
+        {
+            if (a->expo < b->expo)
+            {
+                tempe = a->expo;
+                tempc = a->coeff;
+                a->coeff = b->coeff;
+                a->expo = b->expo;
+                b->coeff = tempc;
+                b->expo = tempe;
+            }
+            b = b->next;
+        }
+        a = a->next;
+    }
+    display(c);
+    a = c;
+    while (a != NULL)
+    {
+        b = a->next;
+        while (b != NULL)
+        {
+            if (a->expo == b->expo)
+            {
+                a->coeff = a->coeff + b->coeff;
+                a->next = b->next;
+                current = b;
+                b = b->next;
+                free(current);
+                continue;
+            }
+            b = b->next;
+            //free(current);
+        }
+        a = a->next;
+    }
+    display(c);
+    return c;
+}
 
-   int main(){
-	int coeff,expo,length1,length2,operation;
-	struct node* head1 = NULL;
-	struct node* head2 = NULL;
-	struct node* resHead = NULL;
-	char choice;
-
-	do{
-
-		printf("Enter your choice, 1 for addition and 2 for multiplication:");
-		scanf("%d",&operation);
-
-		printf("Enter the length of the first linked list:");
-		scanf("%d",&length1);
-
-		printf("Enter the length of the second linked list:");
-		scanf("%d",&length2);
-
-		printf("Enter the values of first linked list....\n");
-		for(int i=0;i<length1;i++){
-		   printf("Enter the coefficient of the node%d:",i+1);
-		   scanf("%d",&coeff); 
-		   printf("Enter the exponent of the node%d:",i+1);
-		   scanf("%d",&expo);
-
-		   head1 = createNode(head1,coeff,expo);
-		}
-		
-		printf("Enter the values of second linked list....\n");
-		for(int i=0;i<length2;i++){
-		   printf("Enter the coefficient of the node%d:",i+1);
-		   scanf("%d",&coeff); 
-		   printf("Enter the exponent of the node%d:",i+1);
-		   scanf("%d",&expo);
-
-		   head2 = createNode(head2,coeff,expo);
-		}
-
-
-		if(operation == 1){
-		    resHead = addLinkedList(head1,head2);
-		    printf("The first linked list is...\n");
-		    traverseNode(head1);
- 		    printf("\n");
-		   
-		    printf("The second linked list is...\n");
-		    traverseNode(head2);
- 		    printf("\n");
-
-		    printf("The sum of the linked list is...\n");
-		    traverseNode(resHead);
- 		    printf("\n");
-		}
-
-		else if(operation == 2){
-		    resHead = addLinkedList(head1,head2);
-		    printf("The first linked list is...\n");
-		    traverseNode(head1);
- 		    printf("\n");
-		   
-		    printf("The second linked list is...\n");
-		    traverseNode(head2);
- 		    printf("\n");
-
-		    printf("The product of the linked list is...\n");
-		    traverseNode(resHead);
- 		    printf("\n");
-		}
-
-		else{
-		    printf("Enter a valid choice...\n");
-		}
-		
-		printf("Do you want to continue?y/n:");
-		scanf("%s",&choice);
-		head1 = NULL;
-		head2 = NULL;
-		resHead = NULL;
-
-		}
-
-		while(choice != 'n');
-
-
-
-
-	
+void main()
+{
+    int choice;
+    struct element *ptr;
+    while (1)
+    {
+        printf("\nMENU:\n");
+        printf("1.Add two polynomials\n");
+        printf("2.Multiply two polynomials\n");
+        printf("3.Exit\n");
+        printf("\nEnter a choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("\nEnter the first polynomial:\n");
+            p1 = readPoly();
+            printf("\nThe first polynomial is: ");
+            display(p1);
+            printf("\nEnter the second polynomial:\n");
+            p2 = readPoly();
+            printf("\nThe second polynomial is: ");
+            display(p2);
+            sum = addPoly(p1, p2);
+            printf("\nThe sum of the given polynomials is: ");
+            display(sum);
+            break;
+        case 2:
+            printf("\nEnter the first polynomial:\n");
+            p1 = readPoly();
+            printf("\nThe first polynomial is: ");
+            display(p1);
+            printf("\nEnter the second polynomial:\n");
+            p2 = readPoly();
+            printf("\nThe second polynomial is: ");
+            display(p2);
+            product = multiPoly();
+            printf("\nThe product of the given polynomials is: ");
+            display(product);
+            break;
+        case 3:
+            exit(0);
+        default:
+            printf("\nPlease enter a valid choice\n");
+            break;
+        }
+        while (sum != NULL)
+        {
+            ptr = sum;
+            sum = sum->next;
+            free(ptr);
+        }
+        while (product != NULL)
+        {
+            ptr = product;
+            product = product->next;
+            free(ptr);
+        }
+        while (p1 != NULL)
+        {
+            ptr = p1;
+            p1 = p1->next;
+            free(ptr);
+        }
+        while (p2 != NULL)
+        {
+            ptr = p2;
+            p2 = p2->next;
+            free(ptr);
+        }
+    }
 }
